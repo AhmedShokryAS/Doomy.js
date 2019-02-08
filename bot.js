6f1c1786ddbd4d52fec349169dfc73615a6fe580
AhmedShokryAS <47435616+AhmedShokryAS@users.noreply.github.com>
2019-02-08 02:57:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,3 +11,20 @@ channel.send(` الشيخ فريزر الي بيشيل باليزر`);
 })
 
 client.login(process.env.BOT_TOKEN);
+
+
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
