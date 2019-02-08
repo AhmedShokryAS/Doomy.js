db0c75c0c727a457fcbc7ffa918f41c70ba03825
AhmedShokryAS <47435616+AhmedShokryAS@users.noreply.github.com>
2019-02-08 03:02:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,13 +18,13 @@ client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(D)) return;
 
-  let command = message.content.split(" ")[0];
+  let command = message.content.split("D")[0];
   command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
+  let args = message.content.split("D").slice(1);
 
   if (command == "say") {
-   message.channel.sendMessage(args.join("  "))
+   message.channel.sendMessage(args.join("D"))
    message.delete()
   }
  });
